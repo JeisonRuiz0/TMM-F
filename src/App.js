a4a432b3c7d44f5926d49c10bdd209a5d42dbe04
JeisonRuiz0 <jeisonruiz.developer@gmail.com>
2023-07-30 19:21:10 -0500

    30-07-2023

--- src/App.js
+++ src/App.js
@@ -5,16 +5,12 @@ import Work from "./Components/Work";
 import Testimonial from "./Components/Testimonial";
 import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
+import HomeSection from "./Sections/HomeSection";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <About />
-      <Work />
-      <Testimonial />
-      <Contact />
-      <Footer />
+      <HomeSection />
     </div>
   );
 }
